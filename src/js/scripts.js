2297bf013a9b86b793119a2ce34ee86cce151f4c
ShengqinHuang <shuang@bu.edu>
2022-11-12 17:21:38 -0500

    progress on main page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,18 @@
 // Add your scripts here
+const parentBgEl = document.querySelector('.bg');
+const childEls = document.querySelectorAll('.container .text span');
+const textEl = document.querySelector('.text');
+
+childEls.forEach((el) => {
+	el.addEventListener('mouseover', (e) => {
+		const id = el.getAttribute('data-id');
+		const bgEl = parentBgEl.querySelector(`.id-${id}`);
+
+		parentBgEl.querySelectorAll('img').forEach((img) => {
+			img.style.display = 'none';
+			img.style.animation = '';
+		});
+		bgEl.style.display = 'block';
+		bgEl.style.animation = 'anima 1s ease forwards';
+	});
+});
