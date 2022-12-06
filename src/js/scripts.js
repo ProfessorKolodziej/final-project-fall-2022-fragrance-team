84f99be624de704f89d1fc48688e11d8afe05d6e
hongchanlee <113472959+hongchanlee@users.noreply.github.com>
2022-12-06 15:24:20 -0500

    change

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -17,3 +17,20 @@ childEls.forEach((el) => {
 		bgEl.style.animation = 'anima 1s ease forwards';
 	});
 });
+
+const url = 'https://api.baserow.io/api/database/rows/table/118884/?user_field_names=true';
+
+const list = document.querySelector('#list');
+
+fetch(url, { headers: { Authorization: `Token hvolOlzFmvicJEK9B6yyyM78Yj4F7G3Z` } })
+  .then(response => response.json())
+  .then(function(data) {
+    for ( let i = 0; i < data.results.length; i++ ) {
+      console.log(data.results[i]);
+      
+      const markup = `<li>
+        <h2>${data.results[i]["Name"]}</h2>
+      </li>`
+      list.insertAdjacentHTML('beforeend', markup);
+    }
+});
