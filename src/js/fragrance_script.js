c865517ed3464234748a95ef6b40dc1a1407f026
hongchanlee <113472959+hongchanlee@users.noreply.github.com>
2022-12-07 21:35:09 -0500

    Update fragrance_script.js

--- src/js/fragrance_script.js
+++ src/js/fragrance_script.js
@@ -33,7 +33,7 @@ function loadRightPicture(className) {
 		mainPic.activeId = className;
 	}
 }
-insideSection = function (classNumber) {
+function insideSection(classNumber) {
 	if (classNumber > 4) return;
 	const showList = ['occation',
 		'fashion',
